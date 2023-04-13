f4064e2de66cb9c7c7e94e41f70e40d455658b0d
sol <ocosio0907@gmail.com>
2023-04-13 18:45:26 +0900

    add dark mode toggle btn

--- src/components/atoms/DarkModeBtn.tsx
+++ src/components/atoms/DarkModeBtn.tsx
@@ -1,13 +1,22 @@
-import { Box, IconButton } from "@chakra-ui/react";
-import { BiRightArrow, BiMoon } from "react-icons/bi";
+import { Box, IconButton, useColorMode } from "@chakra-ui/react";
+import { BiMoon } from "react-icons/bi";
+import { IoMdMoon } from "react-icons/io";
 
 export const DarkModeBtn = () => {
+  const { colorMode, toggleColorMode } = useColorMode();
   return (
     <Box display={"flex"} justifyContent="right" mt={3} mr={3}>
       <IconButton
-        background={"#fff"}
+        background={colorMode === "light" ? "#fff" : "#000"}
         aria-label="dark mode"
-        icon={<BiMoon size={24} />}
+        icon={
+          colorMode === "light" ? (
+            <BiMoon size={24} />
+          ) : (
+            <IoMdMoon fill="yellow" size={24} />
+          )
+        }
+        onClick={toggleColorMode}
       />
     </Box>
   );
