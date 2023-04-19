64c1c3bf0356c7174a6660144255046451b64142
sol <ocosio0907@gmail.com>
2023-04-19 18:16:27 +0900

    fix dark mode text color

--- src/components/atoms/CardTabOne.tsx
+++ src/components/atoms/CardTabOne.tsx
@@ -1,9 +1,12 @@
-import { Box, Heading, Text } from "@chakra-ui/react";
+import { Box, Heading, Text, useColorMode } from "@chakra-ui/react";
 
 export const CardTabOne = () => {
+  const { colorMode } = useColorMode();
   return (
     <Box maxW="32rem">
-      <Heading mb={4}>Hi. Im Shogo Arai.</Heading>
+      <Heading mb={4} color={colorMode === "light" ? "#2F3030" : "#fff"}>
+        Hi. Im Shogo Arai.
+      </Heading>
       <Text fontSize="xl" as="i" color={"#939794"}>
         18 year old. Graduated from N High School.
         <br />
